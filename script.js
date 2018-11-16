817cb3b307e21908146d4e14b1901fdcd99ec7de
st3fanjelo <stefankalenderov@gmail.com>
2018-11-16 18:31:51 +0200

    Created components
    Added main components to the landing page

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $(".headerBackground").height());
       });
   });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= $(".headerBackground").height()) {   
+        $('.goToTop').fadeIn(200);    
+    } else {
+        $('.goToTop').fadeOut(200);   
+    }
+});
+$('.goToTop').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                      
+    }, 1000);
+});
   
 $(function (){ 
-  $("#btn1").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".wrapper").offset().top
-    }, 1500);
-});
-});
+    $("#btn1").click(function() {
+      $('html, body').animate({
+          scrollTop: $(".wrapper").offset().top
+      }, 1500);
+  });
+  });
