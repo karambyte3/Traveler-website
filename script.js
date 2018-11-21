ae9e4902fcf4aba65e8e041d048eab46b911a8bc
st3fanjelo <stefankalenderov@gmail.com>
2018-11-21 17:48:12 +0200

    making hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
+// NAV SCRIPT
+ 
+$(document).ready(function () {
+	$(".hamburger").click(function () {
+		$("li").toggleClass("nav-responsive")
+	})
+});
+
+$(document).ready(function () {
+	$(".hamburger").click(function () {
+		$("nav").toggleClass("responsive-nav-height")
+	})
+});
+
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".navbar-fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $(".headerBackground").height());
       });
   });
+
+  $(function () {
+    $(document).scroll(function () {
+        var $nav = $(".responsive-nav-height");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $(".headerBackground").height());
+      });
+  });
+
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
+
+  // GO TO TOP SCRIPT
 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= $(".headerBackground").height()) {   
